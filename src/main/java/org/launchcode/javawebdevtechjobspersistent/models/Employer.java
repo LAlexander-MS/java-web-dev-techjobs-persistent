4f36c1d5e585db66fbcf29ab9e854c5546c2ac2b
leatia09 <leatia09@icloud.com>
2020-01-28 20:10:21 -0600

    working on controllers section of part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,36 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+        @Size(min = 10, max = 300, message = "Provide Job Location")
+        @NotBlank(message = "Location is required, must be up to 300 characters. Try Again")
+        private String location;
+
+        public Employer(String location) {
+            this.location = location;
+        }
+
+        public Employer() {}
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        @Override
+        public String toString() {
+            return location;
+        }
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,34 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Size(max = 1500, message = "Description cannot exceed 1500 characters")
+//    @NotBlank(message = "Description is required, must be up to 300 characters. Try Again")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    @Override
+//    public String toString() {
+//        return location;
+//    }
 
 }
